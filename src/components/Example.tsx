ac38ed8f56499299a00e7346ffddeeff5b910b2e
Bhanu Teja P <pbteja1998@gmail.com>
2020-10-21 03:03:01 +0530

    Add babel-inline-react-svg and upgrade dependencies

---
+++ src/components/Example.tsx
@@ -0,0 +1,3 @@
+export default function Example() {
+  return <p>Hello World</p>
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
+import Vercel from '../assets/vercel.svg'
+import { Example } from '../components'
+
 export default function Home() {
   return (
     <>
+      <Vercel className="h-4" />
+      <Example />
       <h1 className="text-2xl bg-green-100">Hello World</h1>
       <p className="text-accent-1 ml-2.5">Accent Color Text</p>
     </>
