ea474ce37c8bd592260aa14af826d48aa218131b
Bhanu Teja P <pbteja1998@gmail.com>
2020-11-23 09:20:58 +0530

    Lazy load only the navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,62 +4,63 @@ import Image from 'next/image'
 export default function Home() {
   const [session, loading] = useSession()
 
-  if (loading) {
-    return <div>Loading...</div>
-  }
-
   return (
     <div className="divide-y divide-gray-100">
-      <nav className="p-4">
-        <ul className="flex flex-row space-x-2">
-          <li className="ml-auto">
-            <a
-              className="flex items-center px-4 py-2 space-x-2 rounded-md hover:bg-violet-100"
-              href="https://github.com/pbteja1998/nextjs-starter"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <svg
-                className="w-5 h-5"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
+      {loading ? (
+        <p>loading...</p>
+      ) : (
+        <nav className="p-4">
+          <ul className="flex flex-row space-x-2">
+            <li className="ml-auto">
+              <a
+                className="flex items-center px-4 py-2 space-x-2 rounded-md hover:bg-violet-100"
+                href="https://github.com/pbteja1998/nextjs-starter"
+                target="_blank"
+                rel="noreferrer"
               >
-                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-              </svg>
-              <span className="font-semibold ">GitHub</span>
-            </a>
-          </li>
-          <li>
-            {session ? (
-              <div className="flex items-center space-x-10">
-                <div>
-                  <p>
-                    {' '}
-                    Hello, <b>{session.user.email ?? session.user.name}</b>
-                  </p>
+                <svg
+                  className="w-5 h-5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                </svg>
+                <span className="font-semibold ">GitHub</span>
+              </a>
+            </li>
+            <li>
+              {session ? (
+                <div className="flex items-center space-x-10">
+                  <div>
+                    <p>
+                      {' '}
+                      Hello, <b>{session.user.email ?? session.user.name}</b>
+                    </p>
+                  </div>
+                  <button
+                    onClick={() => signOut()}
+                    className="block px-4 py-2 text-white rounded-md bg-violet-500"
+                  >
+                    Sign Out
+                  </button>
                 </div>
-                <button
-                  onClick={() => signOut()}
-                  className="block px-4 py-2 text-white rounded-md bg-violet-500"
-                >
-                  Sign Out
-                </button>
-              </div>
-            ) : (
-              <>
-                <button
-                  onClick={() => signIn()}
-                  className="block px-4 py-2 text-white rounded-md bg-violet-500"
-                >
-                  Sign In
-                </button>
-              </>
-            )}
-          </li>
-        </ul>
-      </nav>
+              ) : (
+                <>
+                  <button
+                    onClick={() => signIn()}
+                    className="block px-4 py-2 text-white rounded-md bg-violet-500"
+                  >
+                    Sign In
+                  </button>
+                </>
+              )}
+            </li>
+          </ul>
+        </nav>
+      )}
+
       <main>
         <div className="grid grid-cols-1 sm:grid-cols-2 sm:px-8 sm:py-12 sm:gap-x-8 md:py-16">
           <div className="relative z-10 col-start-1 row-start-1 px-4 pt-40 pb-3 bg-gradient-to-t from-black sm:bg-none">
