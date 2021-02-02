742c33bde675889d7251815a520c368cbc622f05
Bhanu Teja Pachipulusu <pbteja1998@gmail.com>
2021-02-02 20:26:29 +0530

    Refactor faunadb setup file

--- src/adapters/fauna/setup.mjs
+++ src/adapters/fauna/setup.mjs
@@ -9,48 +9,40 @@ const client = new faunadb.Client({
 })
 
 async function main() {
-  await client.query(q.CreateCollection({ name: 'accounts' }))
-  await client.query(q.CreateCollection({ name: 'sessions' }))
-  await client.query(q.CreateCollection({ name: 'users' }))
-  await client.query(q.CreateCollection({ name: 'verification_requests' }))
-
   await client.query(
-    q.CreateIndex({
-      name: 'account_by_provider_account_id',
-      source: q.Collection('accounts'),
-      unique: true,
-      terms: [
-        { field: ['data', 'providerId'] },
-        { field: ['data', 'providerAccountId'] },
-      ],
-    })
-  )
-
-  await client.query(
-    q.CreateIndex({
-      name: 'session_by_token',
-      source: q.Collection('sessions'),
-      unique: true,
-      terms: [{ field: ['data', 'sessionToken'] }],
-    })
-  )
-
-  await client.query(
-    q.CreateIndex({
-      name: 'user_by_email',
-      source: q.Collection('users'),
-      unique: true,
-      terms: [{ field: ['data', 'email'] }],
-    })
-  )
-
-  await client.query(
-    q.CreateIndex({
-      name: 'verification_request_by_token',
-      source: q.Collection('verification_requests'),
-      unique: true,
-      terms: [{ field: ['data', 'token'] }],
-    })
+    q.Do(
+      q.CreateCollection({ name: 'accounts' }),
+      q.CreateCollection({ name: 'sessions' }),
+      q.CreateCollection({ name: 'users' }),
+      q.CreateCollection({ name: 'verification_requests' }),
+      q.CreateIndex({
+        name: 'account_by_provider_account_id',
+        source: q.Collection('accounts'),
+        unique: true,
+        terms: [
+          { field: ['data', 'providerId'] },
+          { field: ['data', 'providerAccountId'] },
+        ],
+      }),
+      q.CreateIndex({
+        name: 'session_by_token',
+        source: q.Collection('sessions'),
+        unique: true,
+        terms: [{ field: ['data', 'sessionToken'] }],
+      }),
+      q.CreateIndex({
+        name: 'user_by_email',
+        source: q.Collection('users'),
+        unique: true,
+        terms: [{ field: ['data', 'email'] }],
+      }),
+      q.CreateIndex({
+        name: 'verification_request_by_token',
+        source: q.Collection('verification_requests'),
+        unique: true,
+        terms: [{ field: ['data', 'token'] }],
+      })
+    )
   )
 }
 
