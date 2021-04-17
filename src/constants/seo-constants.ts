cec4edbb7f234e75f170b86ebf5d0dc7edb1dc64
Bhanu Teja Pachipulusu <pbteja1998@gmail.com>
2021-04-18 04:08:05 +0530

    chore: add preview image

--- src/constants/seo-constants.ts
+++ src/constants/seo-constants.ts
@@ -14,7 +14,7 @@ const DEFAULT_DESCRIPTION =
   'A Next.js starter kit template with React 17 + Typescript + Tailwind CSS 2 + React Query 3 + GitHub Auth + Passwordless Auth + Fauna DB.'
 const DEFAULT_CANONICAL = getURL()
 const SITE_NAME = 'Nextjs Starter'
-const DEFAULT_OG_IMAGE = `${DEFAULT_CANONICAL}/twitter-large-card.jpg`
+const DEFAULT_OG_IMAGE = `${DEFAULT_CANONICAL}/preview.png`
 const TWITTER_HANDLE = '@pbteja1998'
 const TWITTER_CARD_TYPE = 'summary_large_image'
 const FAVICON_LINK = '/logo-small.svg'
