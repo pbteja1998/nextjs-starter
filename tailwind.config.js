1d7ff6ebd6a5888dbe86d2106bcaf21513765c1f
Bhanu Teja Pachipulusu <pbteja1998@gmail.com>
2021-04-17 22:04:37 +0530

    chore: add official tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,10 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
