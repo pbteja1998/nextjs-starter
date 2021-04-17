cea84a17adac557e7ca97f742bbc32bc413319c0
Bhanu Teja Pachipulusu <pbteja1998@gmail.com>
2021-04-18 00:25:53 +0530

    chore: add next-seo

---
+++ src/constants/seo-constants.ts
@@ -0,0 +1,32 @@
+export const getURL = (): string => {
+  const url =
+    process?.env?.URL && process.env.URL !== ''
+      ? process.env.URL
+      : process?.env?.VERCEL_URL && process.env.VERCEL_URL !== ''
+      ? process.env.VERCEL_URL
+      : 'http://localhost:3000'
+  return url.includes('http') ? url : `https://${url}`
+}
+
+const DEFAULT_TITLE = ''
+const DEFAULT_TITLE_TEMPLATE = 'Nextjs Starter | %s'
+const DEFAULT_DESCRIPTION =
+  'A Next.js starter kit template with React 17 + Typescript + Tailwind CSS 2 + React Query 3 + GitHub Auth + Passwordless Auth + Fauna DB.'
+const DEFAULT_CANONICAL = getURL()
+const SITE_NAME = 'Nextjs Starter'
+const DEFAULT_OG_IMAGE = `${DEFAULT_CANONICAL}/twitter-large-card.jpg`
+const TWITTER_HANDLE = '@pbteja1998'
+const TWITTER_CARD_TYPE = 'summary_large_image'
+const FAVICON_LINK = '/logo-small.svg'
+
+export const SEO = {
+  DEFAULT_TITLE,
+  DEFAULT_TITLE_TEMPLATE,
+  DEFAULT_DESCRIPTION,
+  DEFAULT_CANONICAL,
+  SITE_NAME,
+  DEFAULT_OG_IMAGE,
+  TWITTER_HANDLE,
+  TWITTER_CARD_TYPE,
+  FAVICON_LINK,
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,60 @@
+import { DefaultSeo } from 'next-seo'
 import type { AppProps } from 'next/app'
+
 import 'tailwindcss/tailwind.css'
+import { SEO } from '../constants/seo-constants'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+const {
+  DEFAULT_TITLE_TEMPLATE,
+  DEFAULT_DESCRIPTION,
+  DEFAULT_CANONICAL,
+  SITE_NAME,
+  DEFAULT_TITLE,
+  DEFAULT_OG_IMAGE,
+  TWITTER_HANDLE,
+  FAVICON_LINK,
+} = SEO
+
+function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
+  const canonicalPath = router.pathname === '/' ? '' : router.pathname
+  const url = `${DEFAULT_CANONICAL}${canonicalPath}`
+  return (
+    <>
+      <DefaultSeo
+        title={DEFAULT_TITLE}
+        titleTemplate={DEFAULT_TITLE_TEMPLATE}
+        description={DEFAULT_DESCRIPTION}
+        canonical={url}
+        openGraph={{
+          type: 'website',
+          locale: 'en_US',
+          url,
+          site_name: SITE_NAME,
+          title: DEFAULT_TITLE,
+          description: DEFAULT_DESCRIPTION,
+          images: [
+            {
+              url: DEFAULT_OG_IMAGE,
+              alt: SITE_NAME,
+            },
+          ],
+        }}
+        twitter={{
+          handle: TWITTER_HANDLE,
+          site: TWITTER_HANDLE,
+          cardType: 'summary_large_image',
+        }}
+        additionalLinkTags={[
+          {
+            rel: 'shortcut icon',
+            href: FAVICON_LINK,
+          },
+        ]}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
