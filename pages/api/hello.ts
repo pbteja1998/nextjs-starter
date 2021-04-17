b2e723c8e896b5501da135c04592a3c5dc23dc10
Bhanu Teja Pachipulusu <pbteja1998@gmail.com>
2021-04-17 21:38:36 +0530

    chore: add typescript

---
+++ pages/api/hello.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  name: string
+}
+
+export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  res.status(200).json({ name: 'John Doe' })
+}
