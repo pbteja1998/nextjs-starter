0b7f8adb0d55e45851092ad8a678951dd8602087
Bhanu Teja Pachipulusu <pbteja1998@gmail.com>
2021-04-17 22:50:05 +0530

    chore: add inter font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,15 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/inter-var-latin.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 module.exports = {
   mode: 'jit',
   purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ['Inter', ...fontFamily.sans],
+    },
     extend: {},
   },
   variants: {
